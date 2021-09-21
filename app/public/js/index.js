f38fd0c578ff59633fb188eeb4a8f6763d6b5644
mschonfe <myschonfeld47@gmail.com>
2021-09-21 18:51:12 -0400

    update js

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,55 +1,38 @@
-const Offer = {
+const SomeApp = {
     data() {
       return {
-        "person": {},
-        "offers": [
-                {
-                    "id": 1,
-                    "name": "Janet Doe",
-                    "salary": 120000,
-                    "bonus": 9000,
-                    "company":"EY",
-                    "offerDate": "2021-09-08"
-                },
-                {
-                    "id": 2,
-                    "name": "Jordan Doe",
-                    "salary": 80000,
-                    "bonus": 2000,
-                    "company":"IU",
-                    "offerDate": "2021-08-09"
-                }
-            ]
-        }
+        result: {},
+        list: [5,6,7,8],
+        message: "Waiting ..."
+      }
     },
     computed: {
         prettyBirthday() {
-            return dayjs(this.person.dob.date)
+            return dayjs(this.result.dob.date)
             .format('D MMM YYYY')
         }
     },
     methods: {
         fetchUserData() {
-            console.log("A");
+            //Method 1:
             fetch('https://randomuser.me/api/')
-            .then( response => response.json() )
-            .then( (responseJson) => {
-                console.log(responseJson);
+            .then(response => response.json())
+            .then((json) => {
+                console.log("Got json back:", json);
+                this.result = json.results[0];
                 console.log("C");
-                this.person = responseJson.results[0];
             })
-            .catch( (err) => {
-                console.error(err);
-            })
-            console.log("B");
+            .catch( (error) => {
+                console.error(error);
+            });
         }
     },
     created() {
         this.fetchUserData();
-    } //end created
-} // end Offer config
-  
-Vue.createApp(Offer).mount('#offerApp');
-console.log("Z");
+    }
 
-//https://github.com/tag/msis-in-class-2021/blob/green-2021/public/js/index.js
+  }
+  
+  Vue.createApp(SomeApp).mount('#someApp');
+
+ // https://github.com/tag/msis-in-class-2021/blob/blue-2021/public/js/index.js
