3d02d7a3a53607745bbf690ba18f5f924a5a4b29
mschonfe <myschonfeld47@gmail.com>
2021-10-29 20:37:34 -0400

    everything

--- app/public/js/books.js
+++ app/public/js/books.js
@@ -1,38 +1,41 @@
-const SomeApp = {
+const booksApp = {
     data() {
       return {
           result:undefined,
           app:0,
           books: [],
-          bookForm:{}
+          bookForm:{},
+          selectedBook: null
       }
     },
-    computed: {
-        prettyBirthday() {
-            return dayjs(this.result.dob.date)
-            .format('D MMM YYYY')
-        }
-    },
     methods: {
+        prettyDollar(n) {
+            const d = new Intl.NumberFormat("en-US").format(n);
+            return "$ " + d;
+        },
         fetchBookData() {
-            //Method 1:
-            fetch('/api/student/')
+            fetch('api/books/')
             .then(response => response.json())
-            .then((json) => {
-                console.log("Got json back:", json);
-                this.books = json;
+            .then((parsedJson) => {
+                console.log(parsedJson);
+                this.books = parsedJson;
+                console.log("C");
             })
             .catch( (error) => {
                 console.error(error);
             })
+            console.log("B");
         },
-        postNewBook(evt) {
-
-            fetch('/api/books/create.php', {
+        postNewBook(evt) { //event handler for form submission, event object is the default 
+            //select the student id and add another offer into this student     
+            console.log("Posting:", this.bookForm);
+            alert("Posting!");
+    
+            fetch('api/books/create.php', {
                 method:'POST',
                 body: JSON.stringify(this.bookForm),
                 headers: {
-                    "Content-Type": "application/json; charset=utf-8"
+                "Content-Type": "application/json; charset=utf-8"
                 }
             })
             .then( response => response.json() )
@@ -45,14 +48,109 @@ const SomeApp = {
                 this.handleResetEdit();
             });
         }
+        postBook(evt) {
+            if (this.selectedBook === null) {
+                this.postNewBook(evt);
+            } else {
+                this.postEditBook(evt);
+            }
+          },
+          postNewBook(evt) {
+            this.bookForm.studentId = this.selectedStudent.id;
+    
+            console.log("Creating!", this.bookForm);
+    
+            fetch('api/book/create.php', {
+                method:'POST',
+                body: JSON.stringify(this.bookForm),
+                headers: {
+                  "Content-Type": "application/json; charset=utf-8"
+                }
+              })
+              .then( response => response.json() )
+              .then( json => {
+                console.log("Returned from post:", json);
+                // TODO: test a result was returned!
+                this.book = json;
+    
+                // reset the form
+                this.resetBookForm();
+              })
+              .catch( err => {
+                alert("Something went horribly wrong!");
+              });
+          },
+          postEditBook(evt) {
+            // this.bookForm.studentId = this.selectedStudent.id;
+            this.bookForm.id = this.selectedBook.id;
+    
+            console.log("Updating!", this.offerForm);
+    
+            fetch('api/book/update.php', {
+                method:'POST',
+                body: JSON.stringify(this.bookForm),
+                headers: {
+                  "Content-Type": "application/json; charset=utf-8"
+                }
+              })
+              .then( response => response.json() )
+              .then( json => {
+                console.log("Returned from post:", json);
+                this.books = json;
+  
+                // reset the form
+                this.handleResetEdit();
+              });
+  
+          },
+          postBook(evt) {
+            console.log ("Test:", this.selectedBook);
+          if (this.selectedBook) {
+              this.postEditBook(evt);
+          } else {
+              this.postNewBook(evt);
+          }
+        },
+          postDeleteBook(book) {
+            if ( !confirm("Are you sure you want to delete this:  "  + "?") ) {
+                return;
+            }
+            console.log("Delete!", book);
+  
+            fetch('api/books/delete.php', {
+              method:'POST',
+              body: JSON.stringify(book),
+              headers: {
+                "Content-Type": "application/json; charset=utf-8"
+              }
+            })
+            .then( response => response.json() )
+            .then( json => {
+              console.log("Returned from post:", json);
+              // TODO: test a result was returned!
+              this.books = json;
+  
+              // reset the form
+              this.handleResetEdit();
+            });
+        },
+
+        handleEditBook(book) {
+            this.selectedBook = book;
+            this.bookForm = Object.assign({}, this.selectedBook);
+        },
+        handleResetEdit() {
+            this.selectedBook = null;
+            this.bookForm = {};
+        }
+
+
     },
     created() {
         this.fetchBookData();
     }
+    
+}
 
-
-  }
   
-  Vue.createApp(SomeApp).mount('#someApp');
-
- // https://github.com/tag/msis-in-class-2021/blob/blue-2021/public/js/index.js
+Vue.createApp(bookApp).mount('#booksApp');
